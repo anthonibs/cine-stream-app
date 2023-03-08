b2e0af198effad51c6844906ce9ddd6afa3db3d4
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-03-08 16:53:43 -0300

    chore: 🔧 Configuração de projeto
    - Adicionando padrões do arquivos do projeto
    - Configurando editorconfig, eslinit e tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 function App() {
-  return (
-    <div className="App">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt velit eveniet laudantium a beatae nihil consequatur. Libero consectetur debitis labore recusandae aspernatur nulla ducimus suscipit, deserunt ad quasi aperiam doloremque?
-    </div>
-  );
+	return (
+		<div className="App">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt velit eveniet laudantium a beatae nihil consequatur. Libero consectetur debitis labore recusandae aspernatur nulla ducimus suscipit, deserunt ad quasi aperiam doloremque?
+		</div >
+	);
 }
 
 export default App;
