e497f2ac6bf4eac4bc74ad6a2d01b1f8d3f09d4b
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-03-11 15:19:20 -0300

    progress: 🚧 Em construção...

--- src/pages/Films/index.tsx
+++ src/pages/Films/index.tsx
@@ -1,17 +1,6 @@
 const Films = () => {
 	return (
-		<div>
-			<h1>Filmes</h1>
-
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste unde voluptate, natus doloremque sit odit esse dolore ad porro dolorum adipisci, dolorem commodi! Repellendus error amet similique laudantium a quaerat.
-			Assumenda, id perspiciatis. Mollitia, unde amet. Quibusdam sit velit at eligendi nobis ullam quas quod repellendus vel sint, ducimus assumenda autem voluptates dignissimos iusto possimus aperiam distinctio consequatur sunt eaque.
-			Nulla facere similique nisi aperiam quia eveniet voluptatum dignissimos ea eaque, minus vel natus cum incidunt enim, hic odio. Eum ullam adipisci minima aspernatur neque rem, accusantium modi non facere.
-			Expedita excepturi repellendus aut atque ab tempore hic! Delectus et molestias sed quae eveniet perferendis temporibus velit sint facilis distinctio! Neque error delectus quia deleniti praesentium, impedit temporibus molestias quod!
-			Veniam magnam, vel pariatur vitae perspiciatis tenetur eius asperiores numquam sint sed! Distinctio laboriosam, minus doloremque unde libero minima! Quod eum dolorum doloribus quae. Facilis quae laudantium deleniti iste a?
-			Sed, accusamus hic! Quaerat sit ipsum neque tempora molestiae beatae consectetur quas! Natus error dolore qui quaerat eum, iste eligendi voluptas laborum consequuntur nihil sunt, perferendis quas officiis nam neque.
-			Unde temporibus tenetur ratione obcaecati maxime porro quod explicabo quibusdam optio provident praesentium minus rem, corrupti odit dolorum. Sunt a doloribus impedit veritatis debitis maxime nobis eaque veniam distinctio quidem.
-			Eos cumque ullam, corrupti quas ipsum voluptas et enim aperiam ut itaque nulla voluptate autem in eum magnam, delectus debitis nesciunt blanditiis magni perspiciatis odit consequatur reprehenderit! Est, sint facere.
-		</div>
+		<h1>Filmes</h1>
 	);
 };
 
