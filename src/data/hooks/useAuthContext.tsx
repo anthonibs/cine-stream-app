bab3a85e128bdd934adf0187f319737fb6a5d4ad
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-03-20 13:46:17 -0300

    feat: ✨ Criada o nome da função para criar novo usuário e messagem de usuário errado

--- src/data/hooks/useAuthContext.tsx
+++ src/data/hooks/useAuthContext.tsx
@@ -5,12 +5,10 @@ import { useNavigate } from 'react-router-dom';
 
 
 export const useAuthContext = () => {
-
 	const { users, authenticated, setAuthenticated, user, setUser } = useContext(AuthContext);
+	const navigate = useNavigate();
 
 	const [loading, setLoading] = useState<boolean>(true);
-
-	const navigate = useNavigate();
 
 	function login(email: string, password: string) {
 		const auth = users.filter(item => item.email === email && item.password == password);
@@ -19,35 +17,40 @@ export const useAuthContext = () => {
 			localStorage.setItem('@auth', JSON.stringify(auth));
 			localStorage.setItem('token', JSON.stringify(Math.random() * 50).toString().slice(5));
 			setAuthenticated(true);
+		} else {
+			alert('Email ou senha estão incorretos.');
 		}
 	}
 
 	function logout() {
 		navigate('/');
+		setAuthenticated(false);
 		localStorage.removeItem('@auth');
 		localStorage.removeItem('token');
-		setAuthenticated(false);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	function registerUser(name: string, email: string, password: string) {
+		// Aqui enviaria para API para cadastrar o usuário no banco de dados
 	}
 
 
 	useEffect(() => {
 		const recoveredUser = localStorage.getItem('@auth');
 		const token = localStorage.getItem('token');
-
 		if (recoveredUser && token) {
 			setAuthenticated(true);
-			setUser(...JSON.parse(recoveredUser));
+			setUser(JSON.parse(recoveredUser)[0]);
 		}
-
 		setLoading(false);
 	}, [setAuthenticated, setUser]);
-
 
 	return {
 		logout,
 		login,
+		registerUser,
 		authenticated,
 		user,
-		loading
+		loading,
 	};
 };
