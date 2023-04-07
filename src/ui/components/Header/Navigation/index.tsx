c868545610fd932a6a9e33ae95b1443c5ac5ba90
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-04-06 23:32:30 -0300

    progress: ♻️ Quebra de linhas de propriedades html

--- src/ui/components/Header/Navigation/index.tsx
+++ src/ui/components/Header/Navigation/index.tsx
@@ -3,36 +3,53 @@ import { NavLink } from 'react-router-dom';
 import { Navbar, NavbarItem, NavbarItems } from './Navigation';
 
 const Navigation = () => {
-
 	return (
 		<Navbar>
 			<NavbarItems>
 				<NavbarItem>
-					<NavLink to='/browser' end className={({ isActive, isPending }) =>
-						isPending ? 'pending' : isActive ? 'active' : ''
-					}>
+					<NavLink
+						to='/browser'
+						data-href='/browser'
+						className={({ isActive, isPending }) =>
+							isPending ? 'pending' : isActive ? 'active' : ''
+						}
+						end
+					>
 						Início
 					</NavLink>
 				</NavbarItem>
 				<NavbarItem>
-					<NavLink to='/browser/films' caseSensitive className={({ isActive, isPending }) =>
-						isPending ? 'pending' : isActive ? 'active' : ''
-					}>
+					<NavLink
+						to='/browser/films'
+						data-href='/browser/films'
+						caseSensitive
+						className={({ isActive, isPending }) =>
+							isPending ? 'pending' : isActive ? 'active' : ''
+						}
+					>
 						Filmes
 					</NavLink>
 				</NavbarItem>
 				<NavbarItem>
-					<NavLink to='/browser/series' className={({ isActive, isPending }) =>
-						isPending ? 'pending' : isActive ? 'active' : ''
-					}>
+					<NavLink
+						to='/browser/series'
+						data-href='/browser/series'
+						className={({ isActive, isPending }) =>
+							isPending ? 'pending' : isActive ? 'active' : ''
+						}
+					>
 						Séries
 					</NavLink>
 				</NavbarItem>
 
 				<NavbarItem>
-					<NavLink to='/browser/my-list' className={({ isActive, isPending }) =>
-						isPending ? 'pending' : isActive ? 'active' : ''
-					}>
+					<NavLink
+						to='/browser/my-list'
+						data-href='/browser/my-list'
+						className={({ isActive, isPending }) =>
+							isPending ? 'pending' : isActive ? 'active' : ''
+						}
+					>
 						Minha lista
 					</NavLink>
 				</NavbarItem>
--- src/ui/components/Logo/index.tsx
+++ src/ui/components/Logo/index.tsx
@@ -1,11 +1,17 @@
-import logo from 'assets/svgs/logo-cine-stream.svg';
 import { memo } from 'react';
+
+import logo from 'assets/svgs/logo-cine-stream.svg';
+
 import { LogoContainer, LogoImage } from './Logo';
 
 const Logo = () => {
 	return (
 		<LogoContainer>
-			<LogoImage src={logo} loading='lazy' alt="Logo oficial da CineStream" />
+			<LogoImage
+				src={logo}
+				loading='lazy'
+				alt="Logo oficial da CineStream"
+			/>
 		</LogoContainer>
 	);
 };
