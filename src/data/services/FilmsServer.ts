68c07c4c6b922cc7d9be9edd1735b7374eec1978
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-04-11 21:21:44 -0300

    ✨ feat:  Filtro de pesquisa de filmes por:
    - Gênero;
    - Ano;
    - Linguagem;
    - Ordenar por (popularidade, avaliação, lançamento e título;

--- src/data/services/FilmsServer.ts
+++ src/data/services/FilmsServer.ts
@@ -1,5 +1,4 @@
 import HttpsServer from './HttpServer';
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 class FilmsServer {
@@ -9,8 +8,12 @@ class FilmsServer {
 		this.httpClient = new HttpsServer('https://api.themoviedb.org/3/');
 	}
 
+	getAllFilms<T>(page: number, language: string, gender: string, sortBy: string, year: string): Promise<T> {
+		return this.httpClient.get(`discover/movie?${API_KEY}&language=${language}&sort_by=${sortBy}&include_adult=false&include_video=false&page=${page}&year=${year}&${gender && `&with_genres=${gender}`}& with_watch_monetization_types=flatrate`);
+	}
+
 	getFilm<T>(id: number, language: string): Promise<T> {
-		return this.httpClient.get(`movie/${id}?${API_KEY}&language=${language}`);
+		return this.httpClient.get(`movie/${id}?${API_KEY}& language=${language} `);
 	}
 }
 
