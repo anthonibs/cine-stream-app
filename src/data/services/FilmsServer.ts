f8cc4b516362d22911593ad4ebdb19c5259a1235
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-04-19 23:18:40 -0300

    ♻️ refactor:  Quebrando linhas da chamada da requisição lista filmes

--- src/data/services/FilmsServer.ts
+++ src/data/services/FilmsServer.ts
@@ -9,11 +9,19 @@ class FilmsServer {
 	}
 
 	getAllFilms<T>(page: number, language: string, gender: string, sortBy: string, year: string): Promise<T> {
-		return this.httpClient.get(`discover/movie?${API_KEY}&language=${language}&sort_by=${sortBy}&include_adult=false&include_video=false&page=${page}&year=${year}&${gender && `&with_genres=${gender}`}& with_watch_monetization_types=flatrate`);
+		return this.httpClient.get(`discover/movie?${API_KEY}
+			&language=${language}
+			&sort_by=${sortBy ? sortBy : 'popularity.desc'}
+			&include_adult=false&include_video=false
+			&page=${page}
+			&year=${year}
+			&${gender && `&with_genres=${gender}`}
+			&with_watch_monetization_types=flatrate`
+		);
 	}
 
 	getFilm<T>(id: number, language: string): Promise<T> {
-		return this.httpClient.get(`movie/${id}?${API_KEY}& language=${language} `);
+		return this.httpClient.get(`movie/${id}?${API_KEY}&language=${language} `);
 	}
 }
 
