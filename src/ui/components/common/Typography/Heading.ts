60bb358075e04dc6ab8d77ae4b7591896c602c88
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-01 23:33:55 -0300

    ✨ feat:  Criando componente Styled para Paragrafo e Títulos
    - Passando o tipo de tag HTML e o tamanho das fonte

---
+++ src/ui/components/common/Typography/Heading.ts
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components';
+
+type IType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle'
+
+interface IHeadingProps {
+	component: IType;
+	variant: IType;
+	color?: 'primary'
+}
+
+const Heading = styled.h1.attrs<IHeadingProps>(({ component }) => ({
+	as: `${component}`
+})) <IHeadingProps>`
+	${({ variant, color }) => css`
+		font-family:  ${({ theme }) => theme.font.family};
+		color: ${({ theme }) => theme.font.color[100]};
+		line-height: 1;
+
+		${variant === 'h1' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.xxl};
+		`}
+
+		${variant === 'h2' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.xl};
+		`}
+
+		${variant === 'h3' && css`
+			font-size:${({ theme }) => theme.font.sizes.h.lg};
+		`}
+
+		${variant === 'h4' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.md};
+		`}
+
+		${variant === 'h5' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.sm};
+		`}
+
+		${variant === 'h6' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.xsm};
+		`}
+
+		${variant === 'subtitle' && css`
+			font-size: ${({ theme }) => theme.font.sizes.h.subtitle};
+		`}
+
+
+		${color && css`
+			color: ${({ theme }) => theme.colors.main};
+		`}
+	`}
+`;
+
+
+export default Heading;
---
+++ src/ui/components/common/Typography/Paragraph.ts
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components';
+
+type IType = 'xxlg' | 'xlg' | 'lg' | 'xmd' | 'md' | 'sm' | 'xsm';
+
+interface IParagraphProps {
+	size?: IType;
+	color?: 'primary' | 'secondary';
+}
+
+const Paragraph = styled.p<IParagraphProps>`${({ size, color }) => css`
+		color: ${({ theme }) => theme.font.color[100]};
+		font-weight: 600;
+		font-size: ${({ theme }) => theme.font.sizes.b.md};
+		font-family: ${({ theme }) => theme.font.family};
+		line-height: normal;
+
+		${size === 'xxlg' && css`
+			font-size:${({ theme }) => theme.font.sizes.b.xxlg};
+		`}
+
+		${size === 'xlg' && css`
+			font-size:${({ theme }) => theme.font.sizes.b.xlg};
+		`}
+
+		${size === 'lg' && css`
+			font-size:${({ theme }) => theme.font.sizes.b.lg};
+		`}
+
+		${size === 'xmd' && css`
+			font-size: ${({ theme }) => theme.font.sizes.b.xmd};
+		`}
+
+		${size === 'sm' && css`
+			font-size: ${({ theme }) => theme.font.sizes.b.md};
+		`}
+
+		${size === 'sm' && css`
+			font-size: ${({ theme }) => theme.font.sizes.b.sm};
+		`}
+
+		${size === 'xsm' && css`
+			font-size: ${({ theme }) => theme.font.sizes.b.xsm};
+		`}
+
+		${color === 'primary' && css`
+			color: ${({ theme }) => theme.colors.main};
+		`}
+
+		${color === 'secondary' && css`
+			color: ${({ theme }) => theme.font.color[200]};
+		`}
+  `}
+`;
+
+
+export default Paragraph;
