3af1f3206619371c8da6c30f7672e2b7e93c36e7
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-05 14:43:35 -0300

    💄 feat:  Adicionando novos cores e propriedades de estilos e tipagem styled.d.ts

--- src/ui/themes/light.ts
+++ src/ui/themes/light.ts
@@ -1,8 +1,7 @@
 export default {
-	header: 'linear-gradient(90.53deg, rgba(0, 10, 22, 0.768) 0.45%, rgba(0, 119, 167, 0.76) 105.51%)',
-	body: '#001D2D',
-	footer: '#292929',
-	menu: 'linear-gradient(182.27deg, #205F85 6.08%, #011422 74.23%)',
+	header: 'linear-gradient(91deg, rgb(0,0,0) 12.8%, rgba(99, 99, 99, 0.392) 100%)',
+	body: '#F5F5F5',
+	footer: '#0B3343',
 
 	font: {
 		weight: {
@@ -11,10 +10,30 @@ export default {
 			300: 600,
 			400: 700
 		},
+		sizes: {
+			h: {
+				xxl: '56px',
+				xl: '48px',
+				lg: '40px',
+				md: '32px',
+				sm: '24px',
+				xsm: '16px',
+				subtitle: '18px'
+			},
+			b: {
+				xxlg: '24px',
+				xlg: '22px',
+				lg: '20px',
+				xmd: '18px',
+				md: '16px',
+				sm: '14px',
+				xsm: '12px'
+			}
+		},
 		family: 'Montserrat, sans-serif',
 		color: {
-			100: '#FFFFFF',
-			200: '#A4A4A4'
+			100: '#393939',
+			200: '#A9A9A9'
 		}
 	},
 
@@ -22,13 +41,18 @@ export default {
 		main: '#09A4E2',
 		basic: {
 			100: '#FFFFFF',
-			200: '#044863',
-			300: '#5C5C5C',
-			400: '#292929'
+			200: '#A9A9A9',
+			300: '#B6B4B4',
+			400: '#0F3F53',
+			500: '#E3E3E3',
+			600: '#8F8F8F'
 		},
-		mainHover: 'rgba(9, 164, 226, 0.7)',
-		hover: {
-			200: 'rgba(4, 72, 99, 0.6)'
+		input: {
+			100: '#FFFFFF'
 		}
+	},
+	team: {
+		100: 'linear-gradient(0deg, rgba(11, 15, 22, 1) 80%, rgba(28, 62, 77, 0) 100%)',
+		200: 'linear-gradient(rgba(180, 180, 180, .43) 0%,  rgba(180, 180, 180, 1) 58%)'
 	}
 };
