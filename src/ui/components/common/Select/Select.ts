85390717256f8b4033a20d5c73137ededdeff118
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-07 21:04:06 -0300

    ✨ feat:  Componente de seleção;
    Criado componente personalizado de selecionar, função que navega pelas setas pra cima e baixo, esc para fechar a lista, com acessibilidade;

--- src/ui/components/common/Select/Select.ts
+++ src/ui/components/common/Select/Select.ts
@@ -1,15 +1,84 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface IOption {
+	focoAtivo?: boolean
+}
+
+interface ISelect {
+	position?: string;
+}
 
 
-export const Selected = styled.select`
-	font-size: .9rem;
-	padding: .575rem .325rem;
-	width: 100%;
-	border-radius: .2rem;
-	border-color: #e4e7eb;
-	color: #212529;
-	background-color: #e4e7eb;
+export const StyledContainer = styled.div`
+	min-width: 100px;
+	position: relative;
 `;
 
-export const Option = styled.option`
+
+export const StyledButton = styled.button`
+	appearance: none;
+	align-items: center;
+	border-radius: .4rem;
+	background-color: ${({ theme }) => theme.colors.basic[500]};
+	cursor: pointer;
+	color:  ${({ theme }) => theme.colors.basic[600]};
+	display: flex;
+	font-size: ${({ theme }) => theme.font.sizes.b.sm};
+	justify-content: space-between;
+	outline: 1px solid  ${({ theme }) => theme.colors.basic[500]};
+	padding: .6rem .4rem;
+	width: 100%;
+
+	& > svg {
+		font-size: 1rem;
+		pointer-events: none;
+		transition: transform .3s ease;
+		transform: rotate(0deg);
+	}
+
+	&.rotate-arrow > svg {
+		transform: rotate(90deg);
+	}
+
+	&.rotate-arrow {
+		border-radius: .4rem .4rem 0 0;
+	}
+`;
+
+
+export const StyledSelect = styled.div<ISelect>`
+	background-color: ${({ theme }) => theme.colors.basic[100]};
+	display: none;
+	outline: 1px solid  ${({ theme }) => theme.colors.basic[500]};
+	position: ${({position}) => position ? position : 'relative'};
+	width: 100%;
+	z-index: 200;
+
+	&.open-item-list {
+		border-radius: 0 0 .4rem .4rem;
+		display: block;
+	}
+`;
+
+
+export const StyledOption = styled.span<IOption>`
+	cursor: pointer;
+	color:  ${({ theme }) => theme.colors.basic[600]};
+	display: block;
+	font-size: ${({ theme }) => theme.font.sizes.b.sm};
+	padding: .4rem;
+	user-select: none;
+
+	&:not(.selected):hover {
+		background-color: ${({ theme }) => theme.colors.basic[500]};
+		opacity: .5;
+	}
+
+	${({ focoAtivo }) => focoAtivo && css`
+		background-color: ${({ theme }) => theme.colors.basic[500]};
+	`}
+
+	&.selected {
+		background-color: ${({ theme }) => theme.colors.basic[500]};
+	}
 `;
--- src/ui/components/common/Select/index.tsx
+++ src/ui/components/common/Select/index.tsx
@@ -1,9 +1,18 @@
-//  React
-import { memo } from 'react';
+//  React e Hooks
+import { memo, useState } from 'react';
 
 // Estilização com styled-components
-import { Option, Selected } from './Select';
+import {
+	StyledOption,
+	StyledContainer,
+	StyledButton,
+	StyledSelect
+} from './Select';
 
+// React Icons
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+// Interfaces
 interface IOption {
 	id: string;
 	name: string;
@@ -13,29 +22,115 @@ interface ISelect<T> {
 	state: T;
 	setState: React.Dispatch<React.SetStateAction<string>>;
 	defaultValue?: string;
+	position?: 'relative' | 'absolute' | 'fixed' | 'static' | 'sticky';
 }
 
-const Select = ({ state, setState, defaultValue }: ISelect<any>) => {
 
-	function handlerSelectValue(value: React.ChangeEvent<HTMLSelectElement>) {
-		setState(value.target.value);
+const Select = ({ state, setState, defaultValue, position }: ISelect<any>) => {
+	const [toggleSelect, setToggleSelect] = useState(false);
+	const [focusedOption, setFocusedOption] = useState<number | null>(null);
+	const [selected, setSelected] = useState('');
+
+	function handlerSelection(event: React.KeyboardEvent<HTMLButtonElement>) {
+		switch (event.key) {
+		case 'ArrowDown':
+			event.preventDefault();
+			setFocusedOption(prevState => {
+				if (prevState === null) {
+					return 0;
+				}
+				if (prevState === state.length - 1) {
+					return state.length - 1;
+				}
+				return prevState += 1;
+			});
+			break;
+
+		case 'ArrowUp':
+			event.preventDefault();
+			setFocusedOption(prevState => {
+				if (!prevState) {
+					return 0;
+				}
+				return prevState -= 1;
+			});
+			break;
+
+		case 'Enter':
+			event.preventDefault();
+			setToggleSelect(!toggleSelect);
+
+			if (focusedOption === null) {
+				return 0;
+			}
+
+			setState(state[focusedOption].id);
+			setSelected(state[focusedOption].name);
+
+			if (selected === state[focusedOption].name) {
+				setState('');
+				setSelected('');
+			}
+			break;
+
+
+		case 'Escape':
+			event.preventDefault();
+			setToggleSelect(!toggleSelect);
+			break;
+
+		default:
+			break;
+		}
 	}
 
+	function handlerSelected(event: IOption) {
+		setState(event.id);
+		setSelected(event.name);
+		setFocusedOption(null);
+		setToggleSelect(!toggleSelect);
+
+		if (selected === event.name) {
+			setState('');
+			setSelected('');
+		}
+	}
+
+
 	return (
-		<Selected
-			onChange={value => handlerSelectValue(value)}
-			defaultChecked
-			defaultValue={defaultValue}
-		>
-			<Option value="" >Sem filtro</Option>
-			{state?.map((option: IOption) => (
-				<Option
-					key={option.id}
-					value={option.id}>
-					{option.name}
-				</Option>
-			))}
-		</Selected>
+		<StyledContainer>
+			<StyledButton
+				className={toggleSelect ? 'rotate-arrow' : ''}
+				aria-labelledby={selected}
+				type='button'
+				onClick={() => setToggleSelect(prev => !prev)}
+				onKeyDown={handlerSelection}
+			>
+				{selected || (defaultValue ? defaultValue : 'No selection')}
+				<MdOutlineKeyboardArrowRight />
+			</StyledButton>
+
+			<StyledSelect
+				className={toggleSelect ? 'open-item-list' : ''}
+				role='select'
+				position={position}
+				aria-hidden={!toggleSelect}
+				onMouseLeave={() => setToggleSelect(false)}
+			>
+				{state?.map((option: IOption, index: number) => (
+					<StyledOption
+						key={index}
+						role="option"
+						id={option.name}
+						className={option.name === selected ? 'selected' : ''}
+						focoAtivo={index === focusedOption}
+						onClick={() => handlerSelected(option)}
+					>
+						{option.name}
+					</StyledOption>
+				))}
+			</StyledSelect>
+		</StyledContainer>
 	);
 };
 
