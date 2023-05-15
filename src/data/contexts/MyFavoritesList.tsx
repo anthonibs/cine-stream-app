a32b0c954fbf6e47768ca3080d5a2846c0705323
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-15 14:32:18 -0300

    ♻️ refactor:  Salvar lista de filmes e séries no localStorage
    Nomes da funções com sintaxes do conteúdo armazenado, salvando suas lista de filmes e séries no localStorage

--- src/data/contexts/MyFavoritesList.tsx
+++ src/data/contexts/MyFavoritesList.tsx
@@ -1,30 +1,45 @@
 import { IMovie } from 'data/interfaces/Movie';
+import { ITvMovie } from 'data/interfaces/TvMovie';
 import { createContext, ReactNode, useState } from 'react';
 
 interface IMyFavoritesListChildren {
 	children: ReactNode;
 }
 interface IMyFavoritesListContext {
-	myFavoritesList: IMovie[];
-	setMyFavoritesList: React.Dispatch<React.SetStateAction<IMovie[]>>;
+	listMovie: IMovie[];
+	setListMovie: React.Dispatch<React.SetStateAction<IMovie[]>>;
+	listSerie: ITvMovie[];
+	setListSerie: React.Dispatch<React.SetStateAction<ITvMovie[]>>;
 }
+
 
 export const MyFavoritesContext = createContext<IMyFavoritesListContext>({} as IMyFavoritesListContext);
 MyFavoritesContext.displayName = 'Wishlist';
 
 export const MyFavoritesProvider = ({ children }: IMyFavoritesListChildren) => {
-	const [myFavoritesList, setMyFavoritesList] = useState<IMovie[]>(() => {
-		const savedList = localStorage.getItem('@my-list');
+	const [listMovie, setListMovie] = useState<IMovie[]>(() => {
+		const savedList = localStorage.getItem('@my-list:movie');
 		if (!savedList) {
 			return [];
 		}
 		return JSON.parse(savedList);
 	});
 
+	const [listSerie, setListSerie] = useState<ITvMovie[]>(() => {
+		const savedList = localStorage.getItem('@my-list:serie');
+		if (!savedList) {
+			return [];
+		}
+		return JSON.parse(savedList);
+	});
+
+	
 	return (
 		<MyFavoritesContext.Provider value={{
-			myFavoritesList,
-			setMyFavoritesList,
+			listMovie,
+			setListMovie,
+			listSerie,
+			setListSerie
 		}}>
 			{children}
 		</MyFavoritesContext.Provider>
--- src/data/hooks/useMyFavoritesList.tsx
+++ src/data/hooks/useMyFavoritesList.tsx
@@ -1,30 +1,48 @@
 import { useContext } from 'react';
 import { MyFavoritesContext } from 'data/contexts/MyFavoritesList';
 import { IMovie } from 'data/interfaces/Movie';
+import { ITvMovie } from 'data/interfaces/TvMovie';
 
 export const useMyFavoritesList = () => {
 
-	const { myFavoritesList, setMyFavoritesList } = useContext(MyFavoritesContext);
+	const { listMovie, setListMovie, listSerie, setListSerie } = useContext(MyFavoritesContext);
 
 	function handlerAddFavoritesList(favorite: IMovie) {
-		const isExist = myFavoritesList.some(item => item.id === favorite.id);
-		const newMyFavorites = myFavoritesList.filter(item => item.id !== favorite.id);
+		const isExist = listMovie.some(item => item.id === favorite.id);
+		const newMyFavorites = listMovie.filter(item => item.id !== favorite.id);
 
 		if (!isExist) {
-			setMyFavoritesList(prev => [...prev, { ...favorite, isFavorite: true }]);
-			localStorage.setItem('@my-list', JSON.stringify([...myFavoritesList, { ...favorite, isFavorite: true }]));
+			setListMovie(prev => [...prev, { ...favorite, isFavorite: true }]);
+			localStorage.setItem('@my-list:movie', JSON.stringify([...listMovie, { ...favorite, isFavorite: true }]));
 		}
 
 		if (isExist) {
-			setMyFavoritesList(newMyFavorites);
-			localStorage.setItem('@my-list', JSON.stringify(newMyFavorites));
+			setListMovie(newMyFavorites);
+			localStorage.setItem('@my-list:movie', JSON.stringify(newMyFavorites));
+		}
+	}
+
+
+	function handlerAddFavoritesListOfSerie(favorite: ITvMovie) {
+		const isExist = listSerie.some(item => item.id === favorite.id);
+		const newMyFavorites = listSerie.filter(item => item.id !== favorite.id);
+
+		if (!isExist) {
+			setListSerie(prev => [...prev, { ...favorite, isFavorite: true }]);
+			localStorage.setItem('@my-list:serie', JSON.stringify([...listSerie, { ...favorite, isFavorite: true }]));
+		}
+
+		if (isExist) {
+			setListSerie(newMyFavorites);
+			localStorage.setItem('@my-list:serie', JSON.stringify(newMyFavorites));
 		}
 	}
 
 
 	return {
-		myFavoritesList,
-		setMyFavoritesList,
+		listMovie,
 		handlerAddFavoritesList,
+		listSerie,
+		handlerAddFavoritesListOfSerie
 	};
 };
