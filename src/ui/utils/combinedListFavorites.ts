e0c713fa282ef29fddde5b391d8df35b5179dee6
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-15 18:00:38 -0300

    ✨ feat:  Função para combinar lista de favoritos com lista de filmes chamada pela api

---
+++ src/ui/utils/combinedListFavorites.ts
@@ -0,0 +1,19 @@
+import { IMovie } from 'data/interfaces/Movie';
+import { ITvMovie } from 'data/interfaces/TvMovie';
+
+function combinedListFavorites<T extends ITvMovie | IMovie>(list: T[], listFavorite: T[]) {
+	const combinedList = list.map((movie: T) => {
+		// Verifica se o filme está na lista de favoritos
+		const isFavorite = listFavorite.some((favorite) => favorite.id === movie.id);
+
+		// Cria um novo objeto com as informações do filme e se ele é um favorito ou não
+		return {
+			...movie,
+			isFavorite: isFavorite,
+		};
+	});
+	return combinedList;
+}
+
+
+export default combinedListFavorites;
