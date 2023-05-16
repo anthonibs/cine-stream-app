88225288180d1442300f7f1ade5d6692fe6e1ffc
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-15 23:20:05 -0300

    ♻️ refactor:  Remover options da requisição de Lista de filmes

--- src/data/services/ListServer.ts
+++ src/data/services/ListServer.ts
@@ -1,7 +1,6 @@
 import HttpsServer from './HttpServer';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 class ListServer {
 	private httpClient: HttpsServer;
@@ -11,12 +10,7 @@ class ListServer {
 	}
 
 	getList<T>(page: number, language: string): Promise<T> {
-		return this.httpClient.get(`/list/${page}?${API_KEY}&language=${language}`, {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${API_TOKEN}`
-			}
-		});
+		return this.httpClient.get(`/list/${page}?${API_KEY}&language=${language}`);
 	}
 }
 
