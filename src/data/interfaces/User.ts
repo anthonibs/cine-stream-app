03741a814d4292be279119b4d4e37adf70891ac9
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-17 23:17:55 -0300

    🏷️ feat:  Adicionando id recebendo string como valor e profile_img pode receber string ou null como valor

--- src/data/interfaces/User.ts
+++ src/data/interfaces/User.ts
@@ -2,8 +2,9 @@ interface IUser {
 	name: string;
 	email: string;
 	password: string;
-	profile_image: string;
+	profile_image: string  | null;
 	notification: number;
+	id: string;
 }
 
 export default IUser;
