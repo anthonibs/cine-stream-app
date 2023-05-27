2fb1968822f81682456fed309906be9429a10258
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-05-26 22:28:33 -0300

    ✨ feat:  Adicionando Funcionalidade Suspense do próprio React
    Alterando os imports comuns para uma função arrow functions para realizar o carregamento lazy, o usuário ter a noção que a tela está carregamento lento.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,29 +1,42 @@
-import { ReactElement } from 'react';
+import { ReactElement, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { useAuthContext } from 'data/hooks/useAuthContext';
+
+import Spinner from 'ui/components/common/Spinner';
+import ScrollRestoration from 'ui/components/ScrollRestoration';
 
 import DefaultPage from 'pages/DefaultPage';
 import DefaultSafeRoutes from 'pages/DefaultSafeRoutes';
 
-import Films from 'pages/Films';
-import MovieDetails from 'pages/Films/MovieDetails';
-import Home from 'pages/Home';
-import MyList from 'pages/MyList';
-import Series from 'pages/Series';
-import TvDetails from 'pages/Series/TvDetails';
-import Signin from 'pages/Signin';
-import Signup from 'pages/Signup';
-import Start from 'pages/Start';
+const Start = lazy(() => import('pages/Start'));
+const Signup = lazy(() => import('pages/Signup'));
+const Films = lazy(() => import('pages/Films'));
+const MovieDetails = lazy(() => import('pages/Films/MovieDetails'));
+const Home = lazy(() => import('pages/Home'));
+const MyList = lazy(() => import('pages/MyList'));
+const Series = lazy(() => import('pages/Series'));
+const TvDetails = lazy(() => import('pages/Series/TvDetails'));
+const Signin = lazy(() => import('pages/Signin'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const PeopleDetails = lazy(() => import('pages/PeopleDetails'));
+const Cast = lazy(() => import('pages/Cast'));
 
 
-import ScrollRestoration from 'ui/components/ScrollRestoration';
-import NotFound from 'pages/NotFound';
-import PeopleDetails from 'pages/PeopleDetails';
-import Cast from 'pages/Cast';
 interface IChildrenProps {
 	children: ReactElement
 }
+
+const StyledContainerSpinner = styled.div`
+	align-items: center;
+	display: flex;
+	height: 100vh;
+	justify-content: center;
+	position: fixed;
+	width: 100%;
+	z-index: 500;
+`;
 
 const AppRoutes = () => {
 	// Rotas privadas caso o usuário não for autenticado pelo login, o usuário sempre fica sendo redirecionado para página de login
@@ -31,7 +44,7 @@ const AppRoutes = () => {
 		const { authenticated, loading } = useAuthContext();
 
 		if (loading) {
-			return <div>Carregando</div>;
+			return <StyledContainerSpinner><Spinner scale={1} /></StyledContainerSpinner>;
 		}
 
 		if (!authenticated) {
@@ -41,32 +54,34 @@ const AppRoutes = () => {
 		return children;
 	};
 
+
 	return (
-		<BrowserRouter>
-			<ScrollRestoration />
-			<Routes >
-				<Route path='/' element={<DefaultPage />}>
-					<Route index element={<Start />} />
-					<Route path='/signin' element={<Signin />} />
-					<Route path='/signup' element={<Signup />} />
+		<Suspense fallback={<StyledContainerSpinner><Spinner scale={1} /></StyledContainerSpinner>}>
+			<BrowserRouter>
+				<ScrollRestoration />
+				<Routes >
+					<Route path='/' element={<DefaultPage />}>
+						<Route index element={<Start />} />
+						<Route path='/signin' element={<Signin />} />
+						<Route path='/signup' element={<Signup />} />
+						{/* Rotas Privadas */}
+						<Route path='/browser' element={<Private><DefaultSafeRoutes /></Private>}>
+							<Route index element={<Home />} />
+							<Route path='films' element={<Films />} />
+							<Route path='films/:slug' element={<MovieDetails />} />
 
-					{/* Rotas Privadas */}
-					<Route path='/browser' element={<Private><DefaultSafeRoutes /></Private>}>
-						<Route index element={<Home />} />
-						<Route path='films' element={<Films />} />
-						<Route path='films/:slug' element={<MovieDetails />} />
+							<Route path='series' element={<Series />} />
+							<Route path='series/:slug' element={<TvDetails />} />
 
-						<Route path='series' element={<Series />} />
-						<Route path='series/:slug' element={<TvDetails />} />
-
-						<Route path='my-list' element={<MyList />} />
-						<Route path='people/:id' element={<PeopleDetails />} />
-						<Route path='cast/:id' element={<Cast />} />
-						<Route path='*' element={<NotFound />} />
+							<Route path='my-list' element={<MyList />} />
+							<Route path='people/:id' element={<PeopleDetails />} />
+							<Route path='cast/:id' element={<Cast />} />
+							<Route path='*' element={<NotFound />} />
+						</Route>
 					</Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+				</Routes>
+			</BrowserRouter>
+		</Suspense>
 	);
 };
 
