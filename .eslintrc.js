22b097b051a3fbf1c52c694c42b0dd0eefc806da
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-06-22 20:15:40 -0300

    🔧 chore:  Configurando eslint e adicionando arquivo .prettierrc na raiz do projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,36 @@
+/* eslint-disable no-undef */
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
+	env: {
+		browser: true,
+		es2021: true,
 	},
-	'extends': [
+	extends: [
 		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react/jsx-runtime',
-		'plugin:react-hooks/recommended'
+		'plugin:react-hooks/recommended',
 	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
 	},
-	'plugins': [
-		'react',
-		'@typescript-eslint',
-		'react-hooks'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'prefer-const':  'off',
+	plugins: ['react', '@typescript-eslint', 'react-hooks'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+		'no-duplicate-imports': 'error',
+		quotes: ['error', 'single'],
+		eqeqeq: ['error', 'always'],
+		'jsx-quotes': ['error', 'prefer-single'],
 		'react-hooks/rules-of-hooks': 'error',
-		'react-hooks/exhaustive-deps': 'warn'
-	}
+		'react-hooks/exhaustive-deps': 'warn',
+		'no-console': 'warn',
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		'linebreak-style': ['error', 'unix'],
+		semi: ['error', 'always'],
+		'prefer-const': 'off',
+	},
 };
