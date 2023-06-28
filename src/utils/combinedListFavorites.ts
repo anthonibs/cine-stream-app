55e3754bb1f6543931f11925de00705eccd9c957
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-06-28 15:50:09 -0300

    ♻️ refactor:  Combinando lista de favoritos com já visto para retornar true/false as propriedades

--- src/utils/combinedListFavorites.ts
+++ src/utils/combinedListFavorites.ts
@@ -1,14 +1,20 @@
-import { IMovie } from 'data/interfaces/Movie';
-import { ITvMovie } from 'data/interfaces/TvMovie';
+import { IAlreadyWatched, IMovie, ITvMovie } from 'data/interfaces';
 
-export function combinedListFavorites<T extends ITvMovie | IMovie>(list: T[], listFavorite: T[]) {
+export function combinedListFavorites<T extends ITvMovie | IMovie>(
+	list: T[],
+	listFavorite: T[],
+	watchList: IAlreadyWatched[]
+) {
 	const combinedList = list?.map((movie: T) => {
 		// Verifica se o filme está na lista de favoritos
 		const isFavorite = listFavorite.some((favorite) => favorite.id === movie.id);
+		const isWatched = watchList.some((watched) => watched.id === movie.id);
+
 		// Cria um novo objeto com as informações do filme e se ele é um favorito ou não
 		return {
 			...movie,
 			isFavorite: isFavorite,
+			alreadyWatched: isWatched,
 		};
 	});
 	return combinedList;
