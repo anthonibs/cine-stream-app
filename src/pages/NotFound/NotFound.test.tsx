be864bb5814c549dd021713aa0bf015c530d6796
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-07-26 12:29:15 -0300

    test:  Teste de renderização página NotFound

---
+++ src/pages/NotFound/NotFound.test.tsx
@@ -0,0 +1,55 @@
+import { LanguageProvider } from 'data/contexts/Language';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '.';
+import { render, renderHook, screen } from '@testing-library/react';
+import useLanguage from 'data/hooks/useLanguage';
+import { ReactNode } from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { ThemeProvider } from 'styled-components';
+import dark from 'ui/themes/dark';
+
+interface NotFoundProps {
+	children: ReactNode;
+}
+
+describe('Must render component <NotFound/>', () => {
+	it('Number max of languages is two', async () => {
+		const wrapper = ({ children }: NotFoundProps) => (
+			<LanguageProvider>{children}</LanguageProvider>
+		);
+
+		const { result } = renderHook(() => useLanguage(), { wrapper });
+
+		expect(result.current.languages).toHaveLength(2);
+	});
+
+	it('Render NotFound', () => {
+		const Provider = () => (
+			<ThemeProvider theme={dark}>
+				<LanguageProvider>
+					<MemoryRouter>
+						<NotFound />
+					</MemoryRouter>
+				</LanguageProvider>
+			</ThemeProvider>
+		);
+
+		render(<Provider />);
+
+		const notFound = screen.getByText('Página não encontrada!');
+		expect(notFound).toBeInTheDocument();
+
+		const goBack = screen.getByRole('button');
+		expect(goBack).toBeInTheDocument();
+	});
+
+	it('Rendering the site in the standard pt-BR language', async () => {
+		const wrapper = ({ children }: NotFoundProps) => (
+			<LanguageProvider>{children}</LanguageProvider>
+		);
+
+		const { result } = renderHook(() => useLanguage(), { wrapper });
+
+		expect(result.current.language).toBe('pt-BR');
+	});
+});
