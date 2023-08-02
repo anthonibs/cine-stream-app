9d37556c5f242553c93d3bf137a3154b6477e950
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-08-01 22:14:49 -0300

    refactor:  Rota my-list fica visível e autorizada somente para usuários logados

--- src/ui/components/Header/Navigation/index.tsx
+++ src/ui/components/Header/Navigation/index.tsx
@@ -6,24 +6,33 @@ import useLanguage from 'data/hooks/useLanguage';
 import * as S from './styles';
 
 import translationNav from './translation.json';
+import useAuthContext from 'data/hooks/useAuthContext';
 
 const Navigation = () => {
 	const { language } = useLanguage();
+	const { authenticated } = useAuthContext();
 
 	const translations = useMemo(() => {
-		return translationNav.translation.find((item) => item.language === language);
+		return translationNav.translation.find((item) => item.iso_code === language);
 	}, [language]);
 
 	return (
 		<S.Navbar>
 			<S.NavbarItems>
-				{translations?.navigation.map((translation) => (
-					<S.NavbarItem key={translation.id}>
-						<NavLink end to={`browser/${translation.slug}`}>
-							{translation.name}
-						</NavLink>
-					</S.NavbarItem>
-				))}
+				{translations?.navigation.map((navigation) => {
+					const { id, name, slug, private: isPrivate } = navigation;
+
+					// Verifica se o item de navegação é público ou se é privado e o usuário está autenticado
+					const shouldRenderLink = !isPrivate || (isPrivate && authenticated);
+
+					return shouldRenderLink ? (
+						<S.NavbarItem key={id}>
+							<NavLink end to={`browser/${slug}`}>
+								{name}
+							</NavLink>
+						</S.NavbarItem>
+					) : null;
+				})}
 			</S.NavbarItems>
 		</S.Navbar>
 	);
